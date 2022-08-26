03fc05e44dbbc20af8da49a5b26250d8c61e2f6e
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2022-08-26 08:45:46 -0400

    Add files via upload

---
+++ src/main/java/com/gohkenytp/primitivemobsr/client/ModModelLayers.java
@@ -0,0 +1,10 @@
+package com.gohkenytp.primitivemobsr.client;
+
+import com.gohkenytp.primitivemobsr.PrimitiveMobsR;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.resources.ResourceLocation;
+
+public class ModModelLayers {
+	public static ModelLayerLocation DODO = new ModelLayerLocation(new ResourceLocation(PrimitiveMobsR.MOD_ID, "dodo"), "dodo");
+	public static ModelLayerLocation GROVE_SPRITE = new ModelLayerLocation(new ResourceLocation(PrimitiveMobsR.MOD_ID, "grove_sprite"), "grove_sprite");
+}
